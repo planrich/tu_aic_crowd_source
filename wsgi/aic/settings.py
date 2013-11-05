24a9e2d8892e67792f7ce088dfe08b505e8beec4
KonradMSteiner <konradm.steiner@gmail.com>
2013-11-05 22:02:05 +0100

    changed callback link

--- wsgi/aic/settings.py
+++ wsgi/aic/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = "klasfiasdf12kCF(uL>ASJ123r5b129cfujxzl;kjashb124e12edljcv"
 if os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL') is not None:
     DB_URL = os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL', 'postgresql://aic:aic@localhost/aic')
     POST_TASK_LINK = 'http://aic13lab2topic2-mobileworks.rhcloud.com/tasks'
-    CALLBACK_LINK = 'http://aic13lab2topic2-mobileworks.rhcloud.com/webhook'
+    CALLBACK_LINK = 'http://main-mobileworks.rhcloud.com/webhook'
 else:
     DB_URL = os.environ.get('DB_URL', 'postgresql://aic:aic@localhost/aic')
     POST_TASK_LINK = 'http://127.0.0.1:5000/tasks'
