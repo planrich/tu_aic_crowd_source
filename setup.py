f7fd8abc61027ba18754069327ac04d5a86dfa54
Richard Plangger <planrichi@gmail.com>
2014-01-16 13:59:07 +0100

    added flask paginate to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='AIC Crowd WebApp',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask', 'psycopg2', 'requests', 'sqlalchemy', 'Jinja2', 'MarkupSafe', 'Werkzeug', 'ago', 'argparse','itsdangerous','wsgiref'],
+      install_requires=['Flask', 'psycopg2', 'requests', 'sqlalchemy', 'Jinja2', 'MarkupSafe', 'Werkzeug', 'ago', 'argparse','itsdangerous','wsgiref','flask-paginate'],
      )
